20e615f56b40c58dd48275fa523957ed44bdf9cf
Thiago Machado Pedro <94803123+ThiagoMachadoPedro@users.noreply.github.com>
2023-05-08 19:24:06 -0300

    teste

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-$(document).ready(function () {
-  $("header button").click(function () {//pega o botao dentro do header e add um evendo a ele, pega o formulario coloca dentro desse evento
-$('form').slideDown(); //slidedown desa barra de navegação
+$(document).ready(function () {});
 
-  });
-$('#botao-cancelar').click(function () {
-$('form').slideUp();
+$("form").on("submit", function (e) {
+  e.preventDefault();
 
-});
+  const tarefasR = $("#tarefas").val();
 
+  const novoItem = $("<li></li>");
+  $(`<h3 id="taref">${tarefasR}<h3/>`).appendTo(novoItem);
 
-  $("form").on("submit", function (e) {
-    e.preventDefault();
+  $(`<div class="container">
+  <h3 "${tarefasR}"></h3>
+  </div>`).appendTo(novoItem);
+  $(novoItem).appendTo("ul");
+  $("#tarefas").val("");
 
-    const enderecoImagem = $("#imagem-nova").val();
-
-    const novoItem = $("<li style='display:none'></li>"); //append inbsere aqui o elemento
-    $(`<img src="${enderecoImagem}" />`).appendTo(novoItem); //appento insere o novo elementona teg da lista
-
-    $(`<div class="overlay">
-    <a href="${enderecoImagem}" target="_blank"
-    title="">
-    Ver Imagem Tamanho Real</a>
-    </div>`).appendTo(novoItem);
-    $(novoItem).appendTo("ul");
-    $(novoItem).fadeIn(1000);
-    $('#imagem-nova').val('');
+  $(novoItem).click(function () {
+    $(this).attr("class", "risco");
   });
 });
